068de64bd64e3f9e3cc49ec96e269b2b4457d623
jen2 <sipila.jen@gmail.com>
2017-04-30 10:05:04 -0400

    Starter project set up done

--- Core-Data-Demo/ViewController.swift
+++ Core-Data-Demo/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var taskTableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var tasks:Array<String> = Array()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        taskTableView.delegate = self
+        taskTableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        if textField.text != "" {
+            self.tasks.append(textField.text!)
+            taskTableView.reloadData()
+        } else {
+            //Add Alert: "Please add a task name."
+            print("Please add a task name")
+        }
     }
-
-
+    
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as! TaskListTableViewCell
+        cell.taskLabel.text = self.tasks[indexPath.row]
+        return cell
+    }
 }
 
